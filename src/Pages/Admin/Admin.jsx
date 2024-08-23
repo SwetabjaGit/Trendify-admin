b8ac6884000a0677f2f1a22e21eb733ff47ec3e8
SwetabjaGit <13stabjahazra@gmail.com>
2024-08-24 00:07:43 +0530

    Added environment variable

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './Admin.css';
 import { Routes,Route } from 'react-router-dom';
@@ -6,6 +6,10 @@ import AddProduct from '../../Components/AddProduct/AddProduct';
 import ListProduct from '../../Components/ListProduct/ListProduct';
 
 const Admin = () => {
+  useEffect(() => {
+    console.log('Env: ', import.meta.env.VITE_BASE_URL);
+  }, []);
+
   return (
     <div className='admin'>
       <Sidebar/>
